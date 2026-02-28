c32b59ed91e55cada46cc5ea4fbb494803a7e7e1
janine737407 <janine737407@gmail.com>
2026-02-28 01:04:25 -0800

    Attempt at Data Preparation

--- image_display.py
+++ image_display.py
@@ -4,13 +4,18 @@ from PIL import Image
 import os
 import math
 import numpy as np
+from torchvision.transforms import v2
+import splitfolders
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+import torch
 
 DATA_DIR = './ddidiversedermatologyimages'
 CSV_FILENAME = 'ddi_metadata.csv' 
 IMAGE_COL = 'DDI_file'            
 LABEL_COL = 'malignant'         
 NUM_IMAGES = 100
-
+"""
 def visualize_dataset(data_dir, csv_file, img_col, label_col, num_images):
     csv_path = os.path.join(data_dir, csv_file)
     
@@ -65,3 +70,32 @@ def visualize_dataset(data_dir, csv_file, img_col, label_col, num_images):
     
 # Run the function
 visualize_dataset(DATA_DIR, CSV_FILENAME, IMAGE_COL, LABEL_COL, NUM_IMAGES)
+"""
+# Data augmentation list
+# , v2.RandomHorizontalFlip(0.5), v2.RandomPerspective(0.5, 0.5)
+# ^ I commented this out because I am just trying to figure out what the rgb values should be for the mean for v2.Normalize()
+# basically make the code below work and then whatever is printed out for the mean/std input it for the v2.Normalize tranform
+transforms = v2.Compose([v2.ToTensor(), v2.Resize((950, 550))])
+
+# Data preparation for training loop
+root = "DDI"
+splitfolders.ratio(root, output="DDI", ratio=(0.6, 0.2, 0.2))
+
+train_data = ImageFolder(os.path.join(root, "train"), transform=transforms)
+
+train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+
+channels_sum = torch.zeros(3)
+channels_squared_sum = torch.zeros(3)
+num_batches = 0
+
+for data, _ in train_loader:
+    channels_sum += torch.mean(data, dim=[0, 2, 3])
+    channels_squared_sum += torch.mean(data**2, dim=[0, 2, 3])
+    num_batches += 1
+
+mean = channels_sum / num_batches
+std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
+
+print(f"Mean: {mean}")
+print(f"Std: {std}")
